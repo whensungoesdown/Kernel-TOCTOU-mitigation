854a0305e4d534ee552bfd7a150e8c844815e94f
uty <whensungoes@gmail.com>
2020-05-21 13:20:37 -0400

    revise abstract, change format to IEEEtran.cls
    missing several package, so errors when using IEEEtran.cls
    
    algorithm has a lot of issues, no $ in "if" "else" "return"

---
+++ clean.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm *.aux *.bbl *.fdb_latexmk *.fls *.log *.pdf *.synctex* *.blg
+rm sources/*.aux sources/*.bbl sources/*.fdb_latexmk sources/*.fls sources/*.log sources/*.pdf sources/*.synctex* sources/*.blg
+rm common/*.aux common/*.bbl common/*.fdb_latexmk common/*.fls common/*.log common/*.pdf common/*.synctex* common/*.blg
+
